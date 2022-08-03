428717cc67050920d370c8bd37989c55d5bff4f0
herosnowman <herosnowmanstudio@gmail.com>
2022-08-03 20:29:11 +0300

    basic version

---
+++ index.js
@@ -0,0 +1,118 @@
+const labelElem = document.querySelector('#label');
+const amountElem = document.querySelector('#amount');
+const submitEntryBtn = document.querySelector('#submitentry');
+const clearAllBtn = document.querySelector('#clearall');
+const itemsList = document.querySelector('#items');
+
+submitEntryBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  saveEntry(labelElem.value, amountElem.value);
+  renderList();
+
+  labelElem.value = "";
+  amountElem.value = "";
+});
+
+clearAllBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  localStorage.removeItem('items');
+
+  renderList();
+});
+
+function saveEntry(label, value) {
+  const items = localStorage.getItem('items');
+
+  let data = [];
+
+  if (items) {
+    data = JSON.parse(items);
+  }
+  
+  data.push({ id: data.length, label: label, value: value });
+  localStorage.setItem('items', JSON.stringify(data));
+}
+
+function renderList() {
+  const items = localStorage.getItem('items');
+
+  itemsList.innerHTML = '';
+
+  if (items) {
+    const data = JSON.parse(items);
+
+    data.forEach(({id, label, value}) => {
+      var entry = document.createElement('div');
+
+      var idEl = document.createElement('div');
+      idEl.classList.add('id');
+      idEl.appendChild(document.createTextNode(id));
+
+      var labelEl = document.createElement('div');
+      labelEl.classList.add('label');
+      labelEl.appendChild(document.createTextNode(label));
+
+      var valueEl = document.createElement('div');
+      valueEl.classList.add('value');
+      valueEl.appendChild(document.createTextNode(value));
+
+      var decBtn = document.createElement('button');
+      decBtn.appendChild(document.createTextNode('-'));
+      decBtn.classList.add('dec');
+
+      var incBtn = document.createElement('button');
+      incBtn.appendChild(document.createTextNode('+'));
+      incBtn.classList.add('inc');
+
+      entry.appendChild(idEl);
+      entry.appendChild(labelEl);
+      entry.appendChild(valueEl);
+      entry.classList.add('item');
+
+      var listItem = document.createElement('li');
+      listItem.appendChild(entry);
+      listItem.appendChild(decBtn);
+      listItem.appendChild(incBtn);
+      listItem.classList.add('listitem');
+      itemsList.appendChild(listItem);
+    });
+  }
+
+  addEventListeners();
+}
+
+function addEventListeners() {
+  const listItems = document.querySelectorAll('.listitem');
+
+  listItems.forEach(elem => {
+    elem.querySelector('.dec').addEventListener('click', (e) => {
+      e.preventDefault();
+      modifyElem(elem, -1);
+    });
+
+    elem.querySelector('.inc').addEventListener('click', (e) => {
+      e.preventDefault();
+      modifyElem(elem, 1);
+    });
+  });
+}
+
+function modifyElem(elem, amount) {
+  const idEl = elem.querySelector('.id');
+  const dataId = idEl.innerText;
+
+  const data = JSON.parse(localStorage.getItem('items'));
+
+  let dataValue = data.find(x => x.id == dataId).value;
+
+  dataValue += amount;
+
+  data[data.find(x => x.id == dataId).id].value = dataValue;
+  localStorage.setItem('items', JSON.stringify(data));
+
+  renderList();
+}
+
+renderList();
